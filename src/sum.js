a5a513d957a148af88deab2c55dcbeb3eb406f7e
Vasil Vanchuk <v.vanchuk@tut.by>
2020-11-23 21:23:56 +0200

    Some tests notes

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,9 @@
 export function sum(a, b) {
   return a + b;
 }
+
+export function consolePromptSum() {
+  const a = Number(prompt("a="));
+  const b = Number(prompt("b="));
+  console.log(`${a}+${b}=${a + b}`);
+}
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,7 +1,20 @@
-import { sum } from "./sum";
+import { consolePromptSum } from "./sum";
 
-describe("sum", () => {
-  it("returns sum of params", () => {
-    expect(sum(1, 2)).toBe(3);
+// describe("sum", () => {
+//   xit("returns sum of params", () => {
+//     expect(sum(1, 2)).toBe(3);
+//   });
+// });
+
+// describe('getSeconds', () => {
+//
+
+describe("consolePromptSum", () => {
+  it("logs to console sum of prompt values", () => {
+    const values = ["1", "2"];
+    jest.spyOn(window, "prompt").mockImplementation(() => values.shift());
+    jest.spyOn(console, "log");
+    consolePromptSum();
+    expect(console.log).toHaveBeenCalledWith(`1+2=3`);
   });
 });
